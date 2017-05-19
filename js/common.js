934745ef4539728f00d73bb5f462a2c2b666dd53
satanna <fortuna.me@ukr.net>
2017-05-19 17:43:59 +0300

    added js part2

--- js/common.js
+++ js/common.js
@@ -5,9 +5,12 @@ $( document ).ready(function() {
         selectYears: 15 // Creates a dropdown of 15 years to control year
     });
 
+    getP24Currency();
 });
 
 var my_event;
+var usd;
+var eur;
 
 function setEvent() {
     my_event = prompt('Яка подія?', '');
@@ -30,4 +33,35 @@ function getAnswer () {
 
     answer.innerHTML = name.value + ' has a ' + my_event + ' at ' + date.value + ' somewhere in ' + place.value + '.';
     answer.parentElement.style.display = 'block';
+}
+
+function getP24Currency() {
+    $.ajax({
+        url: 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5',
+        type: 'get',
+        success: function (data) {
+            var currencies = JSON.parse(JSON.stringify(data));
+
+            usd = currencies[2].buy;
+            eur = currencies[0].buy;
+
+
+
+            console.log(eur);
+        },
+        error: function (data) {
+            alert('Error!');
+            console.log(data);
+        }
+    });
+}
+
+function currencyCalculator() {
+    var eur_count = prompt('Кілкість євро', '');
+    var usd_count = prompt('Кілкість долларів', '');
+
+
+    var answer = eur_count + ' euros are equal '+ eur_count * eur +' grn, '+ usd_count +' dollars are equal '+ usd * usd_count +', one euro is equal '+ eur / usd +' dollars.';
+
+    alert( answer );
 }
